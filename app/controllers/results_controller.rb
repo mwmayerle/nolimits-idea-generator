f746894e3b042a729d343b77418fdee6ba991384
Matt Mayerle <mwmayerle@gmail.com>
2018-05-02 22:53:42 -0500

    added to seeds file

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,11 @@
 class ResultsController < ApplicationController
 
 	def index
-		session[:params] = nil
+		session[:id] = nil #this is id because heroku wants that for whatever reason
 	end
 
 	def create
-		session[:params] ||= params
+		session[:id] ||= params
 		manufacturers = Manufacturer.all
 		manufacturers = JSON.parse(manufacturers.to_json)
 		products = manufacturers.map { |manufacturer| manufacturer["products"] }.flatten
