999b7f98977a840b039c0ffa3759f02b48499a2e
Matt Mayerle <mwmayerle@gmail.com>
2018-05-11 19:47:04 -0500

    changed session key name

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,16 +1,16 @@
 class ResultsController < ApplicationController
 
 	def index
-		session[:id] = nil #this is id because heroku wants that for whatever reason
+		session[:preferences] = nil #this is id because heroku wants that for whatever reason
 	end
 
 	def create
-		session[:id] ||= params
+		session[:preferences] ||= params.delete("authenticity_token")
 		manufacturers = Manufacturer.all
 		manufacturers = JSON.parse(manufacturers.to_json)
 		products = manufacturers.map { |manufacturer| manufacturer["products"] }.flatten
-		products = Result.process_params(session[:params], products)
-		@results = Result.pick_attributes(session[:params], products)
+		products = Result.process_params(session[:preferences], products)
+		@results = Result.pick_attributes(session[:preferences], products)
 		render :show
 	end
 
