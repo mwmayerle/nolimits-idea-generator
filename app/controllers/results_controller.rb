f6cdd46954577defa8099745b91c8f3881bc5012
Matt Mayerle <mwmayerle@gmail.com>
2018-05-11 19:51:05 -0500

    change params to ID

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,16 +1,16 @@
 class ResultsController < ApplicationController
 
 	def index
-		session[:preferences] = nil #this is id because heroku wants that for whatever reason
+		session[:id] = nil #this is id because heroku wants that for whatever reason
 	end
 
 	def create
-		session[:preferences] ||= params.delete("authenticity_token")
+		session[:id] ||= params.delete("authenticity_token")
 		manufacturers = Manufacturer.all
 		manufacturers = JSON.parse(manufacturers.to_json)
 		products = manufacturers.map { |manufacturer| manufacturer["products"] }.flatten
-		products = Result.process_params(session[:preferences], products)
-		@results = Result.pick_attributes(session[:preferences], products)
+		products = Result.process_params(session[:id], products)
+		@results = Result.pick_attributes(session[:id], products)
 		render :show
 	end
 
